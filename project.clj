fc80b6e672737165b95aee07cd479ff9dbb2a3ac
Roman Scherer <roman.scherer@nugg.ad>
2013-07-23 13:19:41 +0200

    Inital import.

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject hashids "0.1.0"
+  :description "Generate short hashes from numbers (like YouTube and Bitly)."
+  :url "https://github.com/r0man/hashids.java"
+  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :plugins [[lein-junit "1.1.3"]]
+  :java-source-paths ["src"]
+  :junit ["test"])
